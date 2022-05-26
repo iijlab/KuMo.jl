87c2a61d52c82e33a971c8d33c6799ed0574c520
Documenter.jl <documenter@juliadocs.github.io>
2022-05-26 06:45:01 +0000

    build based on 0260fbb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = KuMo","category":"page"},{"location":"#KuMo","page":"Home","title":"KuMo","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for KuMo.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [KuMo]","category":"page"}]
+}
