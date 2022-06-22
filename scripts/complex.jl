fd5c2604ced5827c025ffc36d0ca124472c7f273
azzaare <jf@baffier.fr>
2022-06-22 16:05:23 +0900

    Tag new version. Update script for complex topology

--- scripts/complex.jl
+++ scripts/complex.jl
@@ -1,44 +1,51 @@
 using KuMo, DataFrames, StatsPlots, CSV, PGFPlotsX, Plots
 
 function scenario_c1()
-	j1 = job(10, 10, 2, 4, 5)
-    π1 = 0.1
+	j1 = job(100, 5, 4, 3, 1)
+    π1 = 0.001
     req1 = [Request(j1, t) for t in π1:π1:10.]
     user1 = user(req1, 3)
 
-	j2 = job(5, 5, 1, 4, 10)
+	j2 = job(1, 1, 0, 4, 2)
     π2 = 0.1
     req2 = [Request(j2, t) for t in π2:π2:10.]
     user2 = user(req2, 3)
 
-    j3 = job(10, 10, 2, 4, 5)
-    π3 = 0.1
-    req3 = [Request(j3, t) for t in π3:π3:10.]
-    user3 = user(req3, 4)
+    _requests = Vector{KuMo.Request{typeof(j1)}}()
 
-	j4 = job(5, 1, 1, 4, 25)
-    π4 = 0.1
-    req4 = [Request(j4, t) for t in π4:π4:10.]
-    user4 = user(req4, 4)
+    L = 1000
+    r = 0.01
+    λ = 1.
+    n = 1
+    δ = j1.duration
+    c = j1.containers
+
+    π1 = λ / r
+    π2 = (2n - λ) / r
+
+    for i in 0:π1+π2
+        for t in i:δ:π1+π2-i
+            i ≤ π1 && push!(_requests, KuMo.Request(j1, t))
+        end
+    end
 
 
     scenario(;
         duration=1000,
         nodes=[
 			Node(100),
-			Node(150),
-			Node(10),
-			Node(10),
+			Node(100),
+			Node(1000),
+			Node(1000),
 		],
         users=[
-            user1,
-            user2,
-            user3,
-            user4,
+            user(_requests, 1)
+            # user1,
+            # user2,
 		],
 	    links=[
-	    	(1, 2, 150.0), (2, 3, 150.0), (3, 1, 50.0), (4, 1, 50.0),
-	        (2, 1, 150.0), (3, 2, 150.0), (1, 3, 50.0), (1, 4, 50.0),
+	    	(1, 3, 200.0), (2, 3, 200.0), (3, 4, 1000.0), (4, 2, 200.0),
+	        (3, 1, 200.0), (3, 2, 200.0), (4, 3, 1000.0), (2, 4, 200.0),
 	    ],
     )
 end
