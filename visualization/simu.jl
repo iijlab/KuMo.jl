78f2891604b6ae24dad5acc8a39abd67037d7dec
Azzaare <jf@baffier.fr>
2022-07-27 23:07:23 +0900

    Add responsive cumsum for total load

--- visualization/simu.jl
+++ visualization/simu.jl
@@ -4,11 +4,20 @@ using KuMo
 using GLMakie
 using DataFrames
 using CSV
+using Colors
 
-function interactive_analysis(df)
+function interactive_analysis(df, norms)
+    a, b, _, d = marks(df)
+    δ = d === nothing ? b : d
+    colors = distinguishable_colors(length(a:δ), [RGB(1, 1, 1), RGB(0, 0, 0)], dropseed=true)
+
     set_theme!(backgroundcolor=:gray90)
     fontsize_theme = Theme(fontsize=20)
     update_theme!(fontsize_theme)
+
+    set_window_config!(;
+        title="KuMo.jl: scenario analysis"
+    )
 
     fig = Figure(resolution=(1200, 1000))
 
@@ -16,85 +25,82 @@ function interactive_analysis(df)
         fig[1, 1];
         title="Interactive analysis",
         xlabel="Time",
-        ylabel="Resource load",
+        ylabel="Resource load (normalized)"
     )
 
-    a, b, _, d = marks(df)
-    δ = d === nothing ? b : d
     _lines = Vector{Any}(fill(nothing, length(a:δ)))
-    toggles = [Toggle(fig, active = i ≤ b) for i in a:δ]
+    toggles = [Toggle(fig, active=i ≤ b) for i in a:δ]
     labels = map(l -> Label(fig, l), names(df)[a:δ])
-    fig[1, 2][1,1] = grid!(hcat(toggles, labels), tellheight = false)
+    fig[1, 2][1, 1] = grid!(hcat(toggles, labels), tellheight=false)
 
     for c in a:δ
         γ = c - a + 1
-        _lines[γ] = lines!(df[!,:instant], df[!, c])
+        _lines[γ] = lines!(df[!, :instant], df[!, c], color=colors[γ])
         connect!(_lines[γ].visible, toggles[γ].active)
     end
 
-    Axis(
+    df_no_norm = deepcopy(df)
+    for (tag, val) in norms
+        df_no_norm[!, Symbol(tag)] = df_no_norm[!, Symbol(tag)] .* val
+    end
+
+    ax2 = Axis(
         fig[2, 1];
         xlabel="Time",
-        ylabel="Total load",
+        ylabel="Total load"
     )
 
     _areas = Vector{Any}(fill(nothing, length(a:δ)))
-    toggles2 = Observable([Toggle(fig, active = i ≤ b) for i in a:δ])
+    toggles2 = [Toggle(fig, active=i ≤ b) for i in a:δ]
     labels2 = map(l -> Label(fig, l), names(df)[a:δ])
-    fig[2, 2][1,1] = grid!(hcat(toggles2[], labels2), tellheight = false)
+    fig[2, 2][1, 1] = grid!(hcat(to_value(toggles2), labels2), tellheight=false)
 
 
-    actives(toggs) = filter(c -> to_value(toggs)[c-a+1].active[], a:δ)
+    actives(toggs) = filter(c -> toggs[c-a+1].active[], a:δ)
 
     function make_ys(toggs)
         C = actives(toggs)
-        M = zeros(size(df, 1), length(C) + 1)
-        for (i, c) in enumerate(C)
-            M[:, i + 1] = df[!, c]
+        M = zeros(size(df_no_norm, 1), length(a:δ) + 1)
+        for (i, c) in enumerate(a:δ)
+            c ∈ C && (M[:, i+1] = df_no_norm[!, c])
         end
         return cumsum(M, dims=2)
     end
 
-    Y = lift(toggles2) do vals
-        make_ys(vals)
+    Y = lift((t.active for t in toggles2)...) do actives...
+        make_ys(toggles2)
     end
 
     @lift begin
-        c = 0
-        for i in a:δ
-            γ = i - a + 1
-            if i ∈ actives($toggles2)
-                c += 1
-                _areas[γ] = band!(df[!,:instant], $Y[:,c], $Y[:,c+1])
-            else
-                _areas[γ] = band!(df[!,:instant], $Y[:,1], $Y[:,1])
-            end
-            connect!(_areas[γ].visible, $toggles2[γ].active)
+        for c in 1:length(a:δ)
+            _areas[c] === nothing || delete!(ax2.scene, _areas[c])
+            _areas[c] = band!(df[!, :instant], $Y[:, c], $Y[:, c+1], color=colors[c])
+            connect!(_areas[c].visible, toggles2[c].active)
         end
     end
 
-    Menu(fig[3,:][1,4], prompt = "Select plot type(s)...",options = ["all", "area", "lines"], width = 300)
-    Menu(fig[3,:][1,3], prompt = "Select output format...",options = ["pdf", "tex/tikz", "png"], width = 300)
-    Textbox(fig[3,:][1,2], placeholder = "file_name", width = 300)
-    b_save = Button(fig[3, :][1,5], label = "save")
+    Menu(fig[3, :][1, 4], prompt="Select plot type(s)...", options=["all", "resource load", "total load"], width=300)
+    Menu(fig[3, :][1, 3], prompt="Select output format...", options=[".pdf", ".tikz", ".png"], width=300)
+    Textbox(fig[3, :][1, 2], placeholder="file_base_name", width=300)
+    b_save = Button(fig[3, :][1, 5], label="save")
 
-    # on(b_save.clicks) do end
-
-    for t in toggles2[]
-        lift(t) do n
-            notify(toggles2)
-        end
-    end
-
-    return fig, df
+    return fig
 end
 
-function interactive_analysis(path::String = joinpath(pwd(), "simu2.txt"))
-    df = DataFrame(CSV.File(path; delim=' ', ignorerepeated=true))
-    return interactive_analysis(df)
+function interactive_analysis(path::String; norms=Dict())
+    df = DataFrame(CSV.File(path; delim=','))
+    return interactive_analysis(df, norms)
 end
 
-function interactive_analysis(s::Scenario = SCENARII[:four_nodes])
+function interactive_analysis(s::Scenario=SCENARII[:four_nodes])
     df = simulate(s, ShortestPath())[2]
     return interactive_analysis(df)
 end
+
+norms = Dict(
+    [
+    "2" => 10,
+    "3" => 10,
+]
+)
+interactive_analysis("simu2.csv"; norms)
--- visualization/simu2.jl
+++ visualization/simu2.jl
@@ -3,11 +3,12 @@
 using GLMakie
 using DataFrames
 using CSV
+using Colors
 
 function interactive_analysis(df)
     df_no_norm = deepcopy(df)
-    df_no_norm[!, 2:3] = df[!,2:3] .* 1
-    df_no_norm[!, 4:5] = df[!,4:5] .* 10
+    df_no_norm[!, 2:3] = df[!, 2:3] .* 1
+    df_no_norm[!, 4:5] = df[!, 4:5] .* 10
 
     set_theme!(backgroundcolor=:gray90)
     fontsize_theme = Theme(fontsize=20)
@@ -18,70 +19,66 @@ function interactive_analysis(df)
     Axis(
         fig[1, 1];
         title="Interactive analysis",
-        ylabel="Resource load",
+        ylabel="Resource load"
     )
 
-
-
     a, b, δ = 2, 5, 9
+    colors = distinguishable_colors(length(a:δ), [RGB(1, 1, 1), RGB(0, 0, 0)], dropseed=true)
     _lines = Vector{Any}(fill(nothing, length(a:δ)))
-    toggles = [Toggle(fig, active = i ≤ b) for i in a:δ]
+    toggles = [Toggle(fig, active=i ≤ b) for i in a:δ]
     labels = map(l -> Label(fig, l), names(df)[a:δ])
-    fig[1, 2] = grid!(hcat(toggles, labels), tellheight = false)
+    fig[1, 2] = grid!(hcat(toggles, labels), tellheight=false)
 
     for c in a:δ
         γ = c - a + 1
-        _lines[γ] = lines!(df[!,Symbol("#time")], df[!, c])
+        _lines[γ] = lines!(df[!, Symbol("#time")], df[!, c], color=colors[γ])
         connect!(_lines[γ].visible, toggles[γ].active)
     end
 
-    Axis(
+    ax2 = Axis(
         fig[2, 1];
         xlabel="Time",
-        ylabel="Total load",
+        ylabel="Total load"
     )
 
     _areas = Vector{Any}(fill(nothing, length(a:δ)))
-    toggles2 = [Observable(Toggle(fig, active = i ≤ b)) for i in a:δ]
+    toggles2 = [Toggle(fig, active=i ≤ b) for i in a:δ]
     labels2 = map(l -> Label(fig, l), names(df)[a:δ])
-    fig[2, 2] = grid!(hcat(map(t -> to_value(t), toggles2), labels2), tellheight = false)
+    fig[2, 2] = grid!(hcat(to_value(toggles2), labels2), tellheight=false)
 
-    actives(toggs) = filter(c -> to_value(toggs[c-a+1]).active, a:δ)
+    @info "debug 1" toggles2 first(toggles2) first(toggles2).active toggles2[1].active[]
+
+    actives(toggs) = filter(c -> toggs[c-a+1].active[], a:δ)
 
     function make_ys(toggs)
+        @info "debug toggs" toggs
         C = actives(toggs)
-        @info "debug Y internal 2"
-        M = zeros(size(df_no_norm, 1), length(C) + 1)
-        for c in C
-            M[:, c] = df_no_norm[!, c]
+        M = zeros(size(df_no_norm, 1), length(a:δ) + 1)
+        for (i, c) in enumerate(a:δ)
+            c ∈ C && (M[:, i+1] = df_no_norm[!, c])
         end
         return cumsum(M, dims=2)
     end
 
-    Y = lift(toggles2) do vals
-        @info "debug Y internal"
-        make_ys(vals)
+    Y = lift((t.active for t in toggles2)...) do actives...
+        @info actives
+        make_ys(toggles2)
     end
 
     @lift begin
-        @info "debug Y" Y
-        c = 0
-        for i in a:δ
-            γ = i - a + 1
-            if i ∈ actives($toggles2)
-                c += 1
-                _areas[γ] = band!(df_no_norm[!,Symbol("#time")], $Y[:,c], $Y[:,c+1])
-            else
-                _areas[γ] = band!(df_no_norm[!,Symbol("#time")], $Y[:,1], $Y[:,1])
-            end
-            # connect!(_areas[γ].visible, $toggles2[γ].active)
+        for c in 1:length(a:δ)
+            _areas[c] === nothing || delete!(ax2.scene, _areas[c])
+            _areas[c] = band!(df_no_norm[!, Symbol("#time")], $Y[:, c], $Y[:, c+1], color=colors[c])
+            connect!(_areas[c].visible, toggles2[c].active)
         end
     end
 
     return fig
 end
 
-function interactive_analysis(path::String = joinpath(pwd(), "simu2.txt"))
+function interactive_analysis(path::String=joinpath(pwd(), "simu2.txt"))
     df = DataFrame(CSV.File(path; delim=' ', ignorerepeated=true))
     return interactive_analysis(df)
 end
+
+interactive_analysis()
