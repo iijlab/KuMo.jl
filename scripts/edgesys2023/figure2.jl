32506fde42515674837645b9da0f87e442898e7b
Azzaare <jf@baffier.fr>
2023-03-10 12:24:59 +0900

    Final pseudo-cost figure

--- scripts/edgesys2023/figure2.jl
+++ scripts/edgesys2023/figure2.jl
@@ -16,19 +16,21 @@ plot_pc = StatsPlots.plot(
         cost_times_pc,
         idle_cost_pc,
     ],
-    0:0.01:0.9;
-    label=["convex" "monotonic" "load +.2" "cost +.5" "cost ×2" "idle cost ×1.5"],
+    0:0.01:0.95;
+    label=["\\bf convex" "\\bf monotonic" "\\em load +.2" "\\em cost +.5" "\\em cost ×2" "\\em idle cost ×1.5"],
     legend=:topleft,
     xlabel="load",
     ylabel="pseudo cost",
     yticks=0:1:10,
-    xticks=0:0.25:1,
+    xticks=0.25:0.25:1,
     xlims=(0.0, 1.0),
     ylims=(0.0, 10.0),
     # w=[2.0, 2.0, 1.0, 1.0, 1.0, 1.0],
     plot_titlefontsize=20,
-    legendfontsize=12,
+    legendfontsize=10,
+    legendtitlefontsize=12,
     labelfontsize=18,
-    line=([2 2 1 1 1 1], [:solid :solid :dash :dot :dashdot :dashdotdot])
+    line=([2.5 2.5 1.25 1.25 1.25 1.25], [:solid :solid :dash :dot :dashdot :dashdotdot])
+    # legendtitle="Pseudo-costs"
 )
 savefig(plot_pc, joinpath(figuresdir(), "figure2-pseudo_costs.pdf"))
