0024aaab80815ef8fabe79c5db5ac029e9a5da93
Azzaare <jf@baffier.fr>
2023-03-10 12:11:52 +0900

    Update plots

--- scripts/edgesys2023/edge.jl
+++ scripts/edgesys2023/edge.jl
@@ -265,130 +265,240 @@ function mini_final_edge(;
     jd=1.0
 )
 
-    # scenario 1: FlatNode -- FlatLink
-    nodes_1 = [
+    flat_nodes = [
         FlatNode(c, flat),
         FlatNode(c, flat),
         FlatNode(c, flat),
     ]
-    links_1_4_5 = [
+
+    convex_nodes = [
+        Node(c),
+        Node(c),
+        Node(c),
+    ]
+
+    monotonic_nodes = [
+        EqualLoadBalancingNode(c),
+        EqualLoadBalancingNode(c),
+        EqualLoadBalancingNode(c),
+    ]
+
+    flat_links = [
         (1, 2, FlatLink(c, flat)),
         (1, 3, FlatLink(c, flat)),
         (2, 3, FlatLink(c, flat)),
     ]
-    s1 = mini_edge_scenario(nodes_1, links_1_4_5; duration, rate, jd, drones, phase)
-    title = "scenario 1: FlatNode -- FlatLink"
-    p1, df1 = simulate_and_plot(s1, ShortestPath(); target=:nodes, plot_type=:areaplot, title)
 
-    # scenario 2: ConvexNode -- MonotonicLink
-    nodes_2_4 = [
-        Node(c),
-        Node(c),
-        Node(c),
-    ]
-    links_2_3 = [
+    monotonic_links = [
         (1, 2, c),
         (1, 3, c),
         (2, 3, c),
     ]
-    s2 = mini_edge_scenario(nodes_2_4, links_2_3; duration, rate, jd, drones, phase)
-    title = "scenario 2: ConvexNode -- MonotonicLink"
-    p2, df2 = simulate_and_plot(s2, ShortestPath(); target=:nodes, plot_type=:areaplot, title)
-
-    # scenario 3: MonotonicNode -- MonotonicLink
-    nodes_3_5 = [
-        EqualLoadBalancingNode(c),
-        EqualLoadBalancingNode(c),
-        EqualLoadBalancingNode(c),
-    ]
-    s3 = mini_edge_scenario(nodes_3_5, links_2_3; duration, rate, jd, drones, phase)
-    title = "scenario 3: MonotonicNode -- MonotonicLink"
-    p3, df3 = simulate_and_plot(s3, ShortestPath(); target=:nodes, plot_type=:areaplot, title)
-
-    # scenario 4: ConvexNode -- FlatLink
-    s4 = mini_edge_scenario(nodes_2_4, links_1_4_5; duration, rate, jd, drones, phase)
-    title = "scenario 4: ConvexNode -- FlatLink"
-    p4, df4 = simulate_and_plot(s4, ShortestPath(); target=:nodes, plot_type=:areaplot, title)
-
-    # scenario 5: MonotonicNode -- FlatLink
-    s5 = mini_edge_scenario(nodes_3_5, links_1_4_5; duration, rate, jd, drones, phase)
-    title = "scenario 5: MonotonicNode -- FlatLink"
-    p5, df5 = simulate_and_plot(s5, ShortestPath(); target=:nodes, plot_type=:areaplot, title)
-
-    # scenario 6: ConvexNode -- FreeLink
-    s6 = mini_edge_scenario(nodes_2_4; duration, rate, jd, drones, phase)
-    title = "scenario 6: ConvexNode -- FreeLink"
-    p6, df6 = simulate_and_plot(s6, ShortestPath(); target=:nodes, plot_type=:areaplot, title)
-
-    # scenario 7: MonotonicNode -- FreeLink
-    s7 = mini_edge_scenario(nodes_3_5; duration, rate, jd, drones, phase)
-    title = "scenario 7: MonotonicNode -- FreeLink"
-    p7, df7 = simulate_and_plot(s7, ShortestPath(); target=:nodes, plot_type=:areaplot, title)
 
 
-    # scenario 8: AdditiveNode -- MonotonicLink
-    nodes_8 = [
-        AdditiveNode(c, -1.0),
-        AdditiveNode(c, -1.0),
-        AdditiveNode(c, -1.0),
-    ]
-    s8 = mini_edge_scenario(nodes_8, links_1_4_5; duration, rate, jd, drones, phase)
-    title = "scenario 8: AdditiveNode -- MonotonicLink"
-    p8, df8 = simulate_and_plot(s8, ShortestPath(); target=:nodes, plot_type=:areaplot, title)
+    resources = Dict([
+        # scenario 1: FlatNode -- FlatLink
+        :flat_flat => (flat_nodes, flat_links),
+        # scenario 2: ConvexNode -- MonotonicLink
+        :convex_monotonic => (convex_nodes, monotonic_links),
+        # scenario 3: MonotonicNode -- MonotonicLink
+        :monotonic_monotonic => (monotonic_nodes, monotonic_links),
+    ])
 
-    # scenario 9: AdditiveNode -- ConvexLink
-    links_9 = [
-        (1, 2, ConvexLink(c)),
-        (1, 3, ConvexLink(c)),
-        (2, 3, ConvexLink(c)),
-    ]
-    s9 = mini_edge_scenario(nodes_2_4, links_9; duration, rate, jd, drones, phase)
-    title = "scenario 9: ConvexNode -- ConvexLink"
-    p9, df9 = simulate_and_plot(s9, ShortestPath(); target=:nodes, plot_type=:areaplot, title)
+    S = Vector{Scenario}()
+    DF = Vector{DataFrame}()
 
-    # scenario 10: Mix Nodes -- MonotonicLink
-    nodes_10 = [
-        AdditiveNode(c, 0.5),
-        EqualLoadBalancingNode(c),
-        Node(c),
-    ]
-    links_2_3 = [
-        (1, 2, c),
-        (1, 3, c),
-        (2, 3, c),
-    ]
-    s10 = mini_edge_scenario(nodes_10, links_2_3; duration, rate, jd, drones, phase)
-    title = "scenario 10: Mix Nodes -- MonotonicLink"
-    p10, df10 = simulate_and_plot(s10, ShortestPath(); target=:nodes, plot_type=:areaplot, title)
+    for (k, r) in resources
+        @info "starting $k"
+        s = mini_edge_scenario(r...; duration, rate, jd, drones, phase)
+        df = simulate(s, ShortestPath();)[2]
+        push!(S, s)
+        push!(DF, df)
+    end
 
-    P = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10]
-    S = [s1, s2, s3, s4, s5, s6, s7, s8, s9, s10]
-    DF = [df1, df2, df3, df4, df5, df6, df7, df8, df9, df10]
+    # s1 = mini_edge_scenario(nodes_1, links_1_4_5; duration, rate, jd, drones, phase)
+    # title = "scenario 1: FlatNode -- FlatLink"
+    # p1, df1 = simulate_and_plot(s1, ShortestPath(); target=:nodes, plot_type=:areaplot, title)
+
+
+    # nodes_2_4 = [
+    #     Node(c),
+    #     Node(c),
+    #     Node(c),
+    # ]
+    # links_2_3 = [
+    #     (1, 2, c),
+    #     (1, 3, c),
+    #     (2, 3, c),
+    # ]
+    # s2 = mini_edge_scenario(nodes_2_4, links_2_3; duration, rate, jd, drones, phase)
+    # title = "scenario 2: ConvexNode -- MonotonicLink"
+    # p2, df2 = simulate_and_plot(s2, ShortestPath(); target=:nodes, plot_type=:areaplot, title)
+
+
+    # nodes_3_5 = [
+    #     EqualLoadBalancingNode(c),
+    #     EqualLoadBalancingNode(c),
+    #     EqualLoadBalancingNode(c),
+    # ]
+    # s3 = mini_edge_scenario(nodes_3_5, links_2_3; duration, rate, jd, drones, phase)
+    # title = "scenario 3: MonotonicNode -- MonotonicLink"
+    # p3, df3 = simulate_and_plot(s3, ShortestPath(); target=:nodes, plot_type=:areaplot, title)
+
+    # # scenario 4: ConvexNode -- FlatLink
+    # s4 = mini_edge_scenario(nodes_2_4, links_1_4_5; duration, rate, jd, drones, phase)
+    # title = "scenario 4: ConvexNode -- FlatLink"
+    # p4, df4 = simulate_and_plot(s4, ShortestPath(); target=:nodes, plot_type=:areaplot, title)
+
+    # # scenario 5: MonotonicNode -- FlatLink
+    # s5 = mini_edge_scenario(nodes_3_5, links_1_4_5; duration, rate, jd, drones, phase)
+    # title = "scenario 5: MonotonicNode -- FlatLink"
+    # p5, df5 = simulate_and_plot(s5, ShortestPath(); target=:nodes, plot_type=:areaplot, title)
+
+    # # scenario 6: ConvexNode -- FreeLink
+    # s6 = mini_edge_scenario(nodes_2_4; duration, rate, jd, drones, phase)
+    # title = "scenario 6: ConvexNode -- FreeLink"
+    # p6, df6 = simulate_and_plot(s6, ShortestPath(); target=:nodes, plot_type=:areaplot, title)
+
+    # # scenario 7: MonotonicNode -- FreeLink
+    # s7 = mini_edge_scenario(nodes_3_5; duration, rate, jd, drones, phase)
+    # title = "scenario 7: MonotonicNode -- FreeLink"
+    # p7, df7 = simulate_and_plot(s7, ShortestPath(); target=:nodes, plot_type=:areaplot, title)
+
+
+    # # scenario 8: AdditiveNode -- MonotonicLink
+    # nodes_8 = [
+    #     AdditiveNode(c, -1.0),
+    #     AdditiveNode(c, -1.0),
+    #     AdditiveNode(c, -1.0),
+    # ]
+    # s8 = mini_edge_scenario(nodes_8, links_1_4_5; duration, rate, jd, drones, phase)
+    # title = "scenario 8: AdditiveNode -- MonotonicLink"
+    # p8, df8 = simulate_and_plot(s8, ShortestPath(); target=:nodes, plot_type=:areaplot, title)
+
+    # # scenario 9: AdditiveNode -- ConvexLink
+    # links_9 = [
+    #     (1, 2, ConvexLink(c)),
+    #     (1, 3, ConvexLink(c)),
+    #     (2, 3, ConvexLink(c)),
+    # ]
+    # s9 = mini_edge_scenario(nodes_2_4, links_9; duration, rate, jd, drones, phase)
+    # title = "scenario 9: ConvexNode -- ConvexLink"
+    # p9, df9 = simulate_and_plot(s9, ShortestPath(); target=:nodes, plot_type=:areaplot, title)
+
+    # # scenario 10: Mix Nodes -- MonotonicLink
+    # nodes_10 = [
+    #     AdditiveNode(c, 0.5),
+    #     EqualLoadBalancingNode(c),
+    #     Node(c),
+    # ]
+    # links_2_3 = [
+    #     (1, 2, c),
+    #     (1, 3, c),
+    #     (2, 3, c),
+    # ]
+    # s10 = mini_edge_scenario(nodes_10, links_2_3; duration, rate, jd, drones, phase)
+    # title = "scenario 10: Mix Nodes -- MonotonicLink"
+    # p10, df10 = simulate_and_plot(s10, ShortestPath(); target=:nodes, plot_type=:areaplot, title)
+
+    # P = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10]
+    # S = [s1, s2, s3, s4, s5, s6, s7, s8, s9, s10]
+    # DF = [df1, df2, df3, df4, df5, df6, df7, df8, df9, df10]
 
     # for (i, p) in enumerate(P)
     #     savefig(p, "mini_final_edge_$i")
     # end
 
-    title = ""
-    π1, _ = simulate_and_plot(s1, ShortestPath(); target=:nodes, plot_type=:areaplot, title)
-    π2, _ = simulate_and_plot(s2, ShortestPath(); target=:nodes, plot_type=:plot, title)
-    π3, _ = simulate_and_plot(s2, ShortestPath(); target=:links, plot_type=:areaplot, title)
-    π4, _ = simulate_and_plot(s3, ShortestPath(); target=:nodes, plot_type=:plot, title)
-    π5, _ = simulate_and_plot(s3, ShortestPath(); target=:links, plot_type=:areaplot, title)
+    # title = ""
+    # π1, _ = simulate_and_plot(s1, ShortestPath(); target=:nodes, plot_type=:areaplot, title)
+    # π2, _ = simulate_and_plot(s2, ShortestPath(); target=:nodes, plot_type=:plot, title)
+    # π3, _ = simulate_and_plot(s2, ShortestPath(); target=:links, plot_type=:areaplot, title)
+    # π4, _ = simulate_and_plot(s3, ShortestPath(); target=:nodes, plot_type=:plot, title)
+    # π5, _ = simulate_and_plot(s3, ShortestPath(); target=:links, plot_type=:areaplot, title)
 
-    Π = [π1, π2, π3, π4, π5]
-    for (i, p) in enumerate(Π)
-        savefig(p, "micro_final_edge_$i.pdf")
-    end
+    # Π = [π1, π2, π3, π4, π5]
+    # for (i, p) in enumerate(Π)
+    #     savefig(p, "micro_final_edge_$i.pdf")
+    # end
 
-    return P, S, DF, Π
+    return S, DF
 end
 
 # P, S = mini_final_edge(; drones=10, rate=0.1, jd=1.0)
-P, S, DF, Π = mini_final_edge(; drones=20, rate=0.1, jd=0.5, flat=1.125, c=100)
+# P, S, DF, Π = mini_final_edge(; drones=20, rate=0.1, jd=0.5, flat=1.125, c=100)
 
 # show_simulation(S[1])
 
-p = StatsPlots.plot(Π...)
+# p = StatsPlots.plot(Π...)
 
-savefig(p, "micro.tikz")
+# savefig(p, "micro.tikz")
+
+
+S, DF = mini_final_edge(; drones=10, rate=0.1, jd=1.0)
+
+S2, DF2 = mini_final_edge(; drones=100, rate=0.1, jd=0.1)
+
+# flat flat
+p_flat_flat = @df DF[2] areaplot(:instant,
+    cols(6:8), xlabel="time", seriestype=:steppre,
+    ylabel="load",
+    linestyle=:solid,
+    w=1.25,
+)
+
+# convex monotonic
+p_convex_monotonic = @df DF[3] plot(:instant,
+    cols(6:8), xlabel="time", seriestype=:steppre,
+    ylabel="load",
+    linestyle=:auto,
+    w=1.25,
+)
+
+
+# monotonic monotonic
+p_monotonic_monotonic = @df DF[1] plot(:instant,
+    cols(6:8), xlabel="time", seriestype=:steppre,
+    ylabel="load",
+    linestyle=:auto,
+    w=1.25,
+)
+
+# all
+p = plot(p_flat_flat, p_convex_monotonic, p_monotonic_monotonic; layout=(3, 1))
+
+# convex monotonic
+p_convex_monotonic2 = @df DF2[3] plot(
+    :instant,
+    cols(6:8),
+    seriestype=:steppre,
+    linestyle=:auto,
+    w=1,
+    ylabel="load",
+)
+
+
+# monotonic monotonic
+p_monotonic_monotonic2 = @df DF2[1] plot(
+    :instant,
+    cols(6:8),
+    seriestype=:steppre,
+    linestyle=:auto,
+    w=1,
+    ylabel="load",
+)
+
+# flat flat
+p_flat_flat2 = @df DF2[2] areaplot(
+    :instant,
+    cols(6:8),
+    seriestype=:steppre,
+    linestyle=:solid,
+    w=0.1,
+    ylabel="total load",
+    xlabel="time",
+)
+
+# all
+p2 = plot(p_flat_flat2, p_convex_monotonic2, p_monotonic_monotonic2; layout=(3, 1))
+
+savefig(joinpath(figuresdir(), "placeholder.pdf"))
--- scripts/edgesys2023/figure2.jl
+++ scripts/edgesys2023/figure2.jl
@@ -1,4 +1,4 @@
-include("common.jl")
+# include("common.jl")
 
 convex_pc = x -> pseudo_cost(1.0, x, Val(:default))
 monotonic_pc = x -> pseudo_cost(1.0, x, Val(:equal_load_balancing))
@@ -23,10 +23,12 @@ plot_pc = StatsPlots.plot(
     ylabel="pseudo cost",
     yticks=0:1:10,
     xticks=0:0.25:1,
-    ylims = (0., 10.),
-    w=1.,
+    xlims=(0.0, 1.0),
+    ylims=(0.0, 10.0),
+    # w=[2.0, 2.0, 1.0, 1.0, 1.0, 1.0],
     plot_titlefontsize=20,
     legendfontsize=12,
-    labelfontsize=18
+    labelfontsize=18,
+    line=([2 2 1 1 1 1], [:solid :solid :dash :dot :dashdot :dashdotdot])
 )
 savefig(plot_pc, joinpath(figuresdir(), "figure2-pseudo_costs.pdf"))
