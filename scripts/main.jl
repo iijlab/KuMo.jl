90b2bcc3e01a699bffa57a302a6161a8fc737292
Jean-Francois Baffier <jf@baffier.fr>
2023-03-22 16:27:04 +0900

    Update main.jl

--- scripts/main.jl
+++ scripts/main.jl
@@ -1,66 +1,95 @@
-## Script to execute to reproduce all the figures in the paper (see README.md)
+# Add this file to ~/.julia/config/ (mkdir config if necessary)
 
-using Pkg
-Pkg.activate(; temp=true)
-Pkg.update()
+try
+    using Revise
+catch e
+    @warn "Error initializing Revise: trying install" exception=(e, catch_backtrace())
+    using Pkg
+    Pkg.add("Revise")
+end
+
+try
+    using LocalRegistry
+catch e
+    @warn "Error initializing LocalRegistry: trying install" exception=(e, catch_backtrace())
+    using Pkg
+    Pkg.add("LocalRegistry")
+end
+
+try
+    using PackageCompatUI
+catch e
+    @warn "Error initializing PackageCompatUI: trying install" exception=(e, catch_backtrace())
+    using Pkg
+    Pkg.add("PackageCompatUI")
+end
+
+try
+    using Term
+    install_term_repr()
+catch e
+    @warn "Error initializing Term: trying install" exception=(e, catch_backtrace())
+    using Pkg
+    Pkg.add("Term")
+end
+
+try
+    using OhMyREPL
+catch e
+    @warn "Error initializing OhMyREPL: trying install" exception=(e, catch_backtrace())
+    using Pkg
+    Pkg.add("OhMyREPL")
+end
 
 try
     using DrWatson
 catch e
-    @warn "Installing DrWatson" exception = (e, catch_backtrace())
+    @warn "Error initializing DrWatson: trying install" exception=(e, catch_backtrace())
+    using Pkg
     Pkg.add("DrWatson")
-    using DrWatson
 end
 
 try
-    using StatsPlots
+    using Coverage
+
 catch e
-    @warn "Installing StatsPlots" exception = (e, catch_backtrace())
-    Pkg.add("StatsPlots")
-    using StatsPlots
+    @error "Error initializing Coverage" exception=(e, catch_backtrace())
+    using Pkg
+    Pkg.add("Coverage")
 end
 
-try
-    using KuMo
-catch e
-    @warn "Installing KuMo" exception = (e, catch_backtrace())
-    Pkg.add(url="https://github.com/Azzaare/KuMo.jl")
-    using KuMo
+function analyze_mallocs(dirs::V) where {S <: AbstractString, V <: AbstractVector{S}}
+    mallocs = collect(Iterators.flatten(map(analyze_malloc, dirs)))
+    sort!(mallocs, lt = Coverage.sortbybytes)
+    return mallocs
 end
 
-const LATEX = "--nolatex" ∉ ARGS
+function subtype_tree(root_type;
+    level = 1, indent = 4, lasts = Dict(1 => true), ancestor = 0
+)
+    if ancestor > 0
+        return subtype_tree(supertype(root_type); indent, ancestor = ancestor - 1)
+    end
+    # Root type printing
+    level == 1 && println(root_type)
 
-if LATEX
-    # Requires luatex (installed by most LaTeX distributions)
-    begin
-        try
-            using PGFPlotsX
-        catch e
-            @warn "Installing PGFPlotsX" exception = (e, catch_backtrace())
-            Pkg.add("PGFPlotsX")
-            using PGFPlotsX
-        end
-        pgfplotsx()
-        latexengine!(PGFPlotsX.LUALATEX)
+    # Determine the correct vertival character
+    vc(lvl) = get!(lasts, lvl, false) ? " " : "│"
+
+    st = subtypes(root_type)
+    for (i, s) in enumerate(st)
+        # Markers for entering and leaving levels
+        i == 1 && setindex!(lasts, false, level)
+        i == length(st) && setindex!(lasts, true, level)
+
+        # Horizontal character
+        hc = get!(lasts, level, false) ? "└" : "├"
+
+        # Actual printing
+        lines = mapreduce(l -> vc(l) * repeat(" ", indent), *, 1:(level-1); init = "")
+        println(lines * hc * "───" * string(s))
+
+        # Next child
+        subtype_tree(s; level = level + 1, indent, lasts)
     end
 end
-
-@quickactivate
-
-function main(; title=true, latex=true)
-    F = [
-        :figure_3,
-        :figure_4,
-        :figure_5,
-        :figure_6,
-        :figure_7,
-        :figure_8,
-    ]
-
-    foreach(f -> figures(f; title, latex), F)
-
-    return nothing
-end
-
-main(; latex=LATEX) # with titles for review
-# main(;title = false, latex=LATEX) # without titles for integration in the paper
